b6059610fa0495953300e10a9a44a9a8d55f895d
rani-98 <nagaranianji1998@gmail.com>
2024-07-15 17:39:36 +0530

    xyz

--- store_world/store_world/settings.py
+++ store_world/store_world/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mobile_store',
+    'mobile_store.apps.MobileStoreConfig',
 
 ]
 
